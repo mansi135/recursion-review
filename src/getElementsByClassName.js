1c0265bf4b93f15578bbd000dd968765808016ba
Hack Reactor Student <student@hackreactor.com>
2017-11-27 15:59:24 -0800

    Completed stringify and getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,46 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  //check current element
+  // iterate iver childs  
+    // recursion
+  var actualNode = document.body;
+
+  var results = [];
+
+  // if (actualNode.classList) {
+  //   if (actualNode.classList.contains(className)) {
+  //     results.concat([actualNode]);
+  //     console.log(results);
+  //     }
+  //   }
+
+  // for (var i = 0; i < actualNode.childNodes.length; i++) {
+  //   results.concat(getElementsByClassName(className, actualNode.childNodes[i]));
+  //   }
+
+  // return results;
+
+  var containsClassName = function(className, actualNode) {
+    if (actualNode.classList) {
+      if (actualNode.classList.contains(className)) {
+        results.push(actualNode);
+        console.log(results);
+      }
+    }
+    
+    console.log(actualNode.hasChildNodes());
+    if (actualNode.hasChildNodes()) {
+      for (var i = 0; i < actualNode.childNodes.length; i++) {
+        containsClassName(className, actualNode.childNodes[i]);
+      }
+    }
+  };
+    
+
+  containsClassName(className,actualNode);
+
+  return results;
+
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,61 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
-};
+ 
+  
+  if (typeof(obj) === 'boolean' || typeof(obj) === 'number' || obj === null) {
+    return "" + obj;
+  }
+
+  if (typeof(obj) === 'string') {
+    return '"' + obj + '"';
+  }
+
+//   if(obj instanceof Function) {
+//     return ;
+// };
+
+  if (Array.isArray(obj)) {
+      if (obj.length === 0) {
+        return '[]';
+      } 
+   
+      var finalString = "[" ;
+      for(var i=0; i < obj.length; i++) {
+        finalString += (stringifyJSON(obj[i]) + ",");       
+      }
+
+      finalString = finalString.slice(0,finalString.length-1);
+
+      finalString += "]";
+
+      return finalString;
+
+    } 
+
+  if (typeof(obj) === 'object') {
+    if (Object.keys(obj).length === 0) {
+      return '{}';
+    } else {
+      var finalString = "{" ;
+    for(var k in obj) {
+      if (!(obj[k] instanceof Function) && !(obj[k] === undefined)) {
+        finalString += stringifyJSON(k) + ':' + stringifyJSON(obj[k]) + ',';
+      }
+    } 
+    if (!(finalString === '{')) {
+      finalString = finalString.slice(0,finalString.length-1);
+    }
+    
+    finalString += "}";
+
+    return finalString;
+    }  
+
+  };
+  
+}
+  
+  
+
+
